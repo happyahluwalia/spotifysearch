6a2ea939bcc433932478f6f89ed078fbb35ffe12
happyahluwalia <happyahluwalia@gmail.com>
2017-05-09 13:51:19 -0400

    Using debounce so that each keystroke does not send a http request

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -2,6 +2,12 @@ import { Artist } from './../../model/Artist';
 import { SportifySearchService } from './../../service/sportifysearch.service';
 import { Component, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/switchMap';
+import { Subject } from 'rxjs/Subject';
+
 @Component({
   selector: 'ha-search',
   templateUrl: './search.component.html',
@@ -10,13 +16,23 @@ import { Component, OnInit } from '@angular/core';
 export class SearchComponent implements OnInit {
   private search: string;
   private searchRes: Artist[];
-  constructor(private spotifyservice: SportifySearchService) { }
+  private searchTerm = new Subject<string>();
+  items: Observable<any[]>;
+
+  constructor(private spotifyservice: SportifySearchService) {
+  }
 
   ngOnInit() {
+    this.searchTerm.debounceTime(500)
+        .distinctUntilChanged()
+        .subscribe((query1: string) => {
+            console.log(query1);
+            this.spotifyservice.searchMusic(query1)
+                    .subscribe(res => this.searchRes = res.artists.items );
+          });
   }
 
  Search(query: string) {
-    this.spotifyservice.searchMusic(query)
-          .subscribe(res => this.searchRes = res.artists.items );
+    this.searchTerm.next(query);
  }
 }
